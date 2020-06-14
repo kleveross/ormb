beb075679df930fc017d3e16f63ac5f87b68f2a0
ZhuYuJin <630268695@qq.com>
2020-06-14 18:52:33 +0800

    support python sdk for ormb (#45)
    * support python sdk for ormb
    
    * feat: Update gitignore and requirements
    
    Co-authored-by: robertzhu <robertzhu@tencent.com>

---
+++ extern-sdk/python/git_release.py
@@ -0,0 +1,58 @@
+import json
+import requests
+import platform
+import sys
+import tarfile
+import os
+
+
+REPOS = "caicloud/ormb"
+VERSION = "latest"
+BIN_PATH = './ormb/bin'
+
+OS_LIST = ["Linux", "Darwin"]
+ARCH_LIST = ["x86_64", "i386"]
+
+
+def untar(fname, dirs):
+    t = tarfile.open(fname)
+    t.extractall(path=dirs)
+
+
+def download():
+    url = 'https://api.github.com/repos/%s/releases/%s' % (REPOS, VERSION)
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        raise Exception("get assets info err, ret code: %s" % r.status_code)
+
+    json_info = json.loads(r.text)
+
+    cur_version = json_info["tag_name"][1:]
+
+    asset_name = "ormb_%s_Linux_x86_64.tar.gz" % cur_version
+    for os_name in OS_LIST:
+        for arch_name in ARCH_LIST:
+            if arch_name in platform.platform().lower() and os_name.lower() in sys.platform:
+                asset_name = "ormb_%s_%s_%s.tar.gz" % (cur_version, os_name, arch_name)
+
+    asset_url = ""
+
+    for asset in json_info["assets"]:
+        if asset_name in asset["browser_download_url"]:
+            asset_url = asset["url"]
+
+    # download the url contents in binary format
+    headers = {'Accept': 'application/octet-stream'}
+    r = requests.get(asset_url, headers=headers)
+
+    # open method to open a file on your system and write the contents
+    with open(asset_name, "wb") as code:
+        code.write(r.content)
+
+    if not os.path.exists(BIN_PATH):
+        os.mkdir(BIN_PATH)
+    untar(asset_name, BIN_PATH)
+
+    os.remove(asset_name)
+
---
+++ extern-sdk/python/ormb/api.py
@@ -0,0 +1,41 @@
+from subprocess import Popen
+from os.path import abspath, join, dirname
+
+BIN_PATH = join(abspath(dirname(__file__)), 'bin')
+
+
+def login(hostname: str, username: str, password: str, insecure_opt: str):
+    ex = Popen([join(BIN_PATH, "ormb"), "login", hostname, "--username", username, "--password", password, "--insecure",
+                insecure_opt])
+    status = ex.wait()
+    return status
+
+
+def push(ref: str):
+    ex = Popen([join(BIN_PATH, "ormb"), "push", ref])
+    status = ex.wait()
+    return status
+
+
+def pull(ref: str):
+    ex = Popen([join(BIN_PATH, "ormb"), "pull", ref])
+    status = ex.wait()
+    return status
+
+
+def export(ref: str, dst: str):
+    ex = Popen([join(BIN_PATH, "ormb"), "export", ref, dst])
+    status = ex.wait()
+    return status
+
+
+def save(src: str, ref: str):
+    ex = Popen([join(BIN_PATH, "ormb"), "save", src, ref])
+    status = ex.wait()
+    return status
+
+
+def remove(ref: str):
+    ex = Popen([join(BIN_PATH, "ormb"), "remove", ref])
+    status = ex.wait()
+    return status
