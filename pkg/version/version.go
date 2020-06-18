ab5ab8d45c92244190237b8c92f239cee75474d9
Ce Gao <gaoce@caicloud.io>
2020-06-18 15:16:36 +0800

    feat: Add version subcommand (#71)
    * feat: Add version subcommand
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>
    
    * fix: Fix tab
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>
    
    * fix: Address the comment
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,6 +4,11 @@
 
 package version
 
+import (
+	"fmt"
+	"runtime"
+)
+
 // Following values should be substituted with a real value during build.
 var (
 	// VERSION is the app-global version string.
@@ -15,3 +20,21 @@ var (
 	// REPOROOT is the app-global repository path string.
 	REPOROOT = "UNKNOWN"
 )
+
+// PrintVersion prints versions from the array returned by Info().
+func PrintVersion() {
+	for _, i := range Info() {
+		fmt.Printf("%v\n", i)
+	}
+}
+
+// Info returns an array of various service versions
+func Info() []string {
+	return []string{
+		fmt.Sprintf("Version: %s", VERSION),
+		fmt.Sprintf("Git SHA: %s", COMMIT),
+		fmt.Sprintf("Repo Root: %s", REPOROOT),
+		fmt.Sprintf("Go Version: %s", runtime.Version()),
+		fmt.Sprintf("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH),
+	}
+}
