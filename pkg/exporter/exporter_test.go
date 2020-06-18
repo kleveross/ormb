8ed1af2094d80908ef5cc710cd0a42489cb8017f
Ce Gao <gaoce@caicloud.io>
2020-06-18 14:58:29 +0800

    feat: Add exporter test
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

---
+++ pkg/exporter/exporter_test.go
@@ -0,0 +1,29 @@
+package exporter_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/caicloud/ormb/pkg/exporter"
+	"github.com/caicloud/ormb/pkg/saver"
+)
+
+var _ = Describe("Exporter", func() {
+	var e exporter.Interface
+
+	BeforeEach(func() {
+		e = exporter.New()
+	})
+
+	It("Should export the model successfully", func() {
+		s := saver.New()
+		pwd := "../../examples/PMML-model"
+
+		m, err := s.Save(pwd)
+		Expect(err).To(BeNil())
+		Expect(m.Metadata.Format).To(Equal("PMML"))
+
+		_, err = e.Export(m, "/tmp")
+		Expect(err).To(BeNil())
+	})
+})
