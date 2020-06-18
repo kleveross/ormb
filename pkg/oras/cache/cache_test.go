c3149863ffaa4d39ade4059886237498c9b4df94
Ce Gao <gaoce@caicloud.io>
2020-06-18 13:58:35 +0800

    feat: Add test for cache (#68)
    * feat: Add test for cache
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>
    
    * feat: Add more test cases
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

---
+++ pkg/oras/cache/cache_test.go
@@ -0,0 +1,68 @@
+package cache
+
+import (
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/caicloud/ormb/pkg/model"
+	"github.com/caicloud/ormb/pkg/oci"
+)
+
+var _ = Describe("Cache", func() {
+	Describe("with real use cases", func() {
+		var c *Cache
+		var err error
+		var rootPath string
+
+		BeforeEach(func() {
+			var i Interface
+			rootPath = ".cache"
+
+			i, err = New(CacheOptRoot(rootPath), CacheOptDebug(true), CacheOptWriter(os.Stdout))
+			c = i.(*Cache)
+		})
+
+		It("Should create the cache successfully", func() {
+			Expect(err).To(BeNil())
+			Expect(c.rootDir).To(Equal(rootPath))
+		})
+
+		Describe("with a cached artifact caicloud/test:v2", func() {
+			var m *model.Model
+			var ref *oci.Reference
+
+			BeforeEach(func() {
+				m = &model.Model{
+					Metadata: &model.Metadata{
+						Format: "SavedModel",
+					},
+					Content: []byte("test12345"),
+				}
+				refStr := "caicloud/test:v1"
+				ref, err = oci.ParseReference(refStr)
+				Expect(err).To(BeNil())
+
+				actual, err := c.StoreReference(ref, m)
+				Expect(err).To(BeNil())
+				Expect(actual.Model).To(Equal(m))
+				Expect(actual.Name).To(Equal(refStr))
+
+				Expect(c.AddManifest(ref, actual.Manifest)).To(BeNil())
+			})
+
+			It("Should list the cached artifact successfully", func() {
+				actual, err := c.ListReferences()
+				Expect(err).To(BeNil())
+				Expect(len(actual)).To(Equal(1))
+			})
+
+			It("Should delete the reference successfully", func() {
+				actual, err := c.DeleteReference(ref)
+				Expect(err).To(BeNil())
+				Expect(actual.Name).To(Equal(ref.FullName()))
+			})
+		})
+	})
+})
